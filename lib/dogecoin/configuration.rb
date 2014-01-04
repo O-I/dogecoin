a4341beb616f85d6b4b255c92c93d7123c646cf2
Rahul Horé <hore.rahul@gmail.com>
2014-01-03 21:07:21 -0500

    Adds a basic configuration.rb

---
+++ lib/dogecoin/configuration.rb
@@ -0,0 +1,15 @@
+module DogeCoin
+  module Configuration
+
+    attr_accessor :api_key
+
+    def configure
+      yield self
+    end
+
+    def reset
+      self.api_key = nil
+      self
+    end
+  end
+end
