9079697e491f25856964677e9dd437419c00145c
Rahul Horé <hore.rahul@gmail.com>
2014-01-07 21:01:26 -0500

    Comments out version.rb for now

--- lib/dogecoin/version.rb
+++ lib/dogecoin/version.rb
@@ -1,18 +1,18 @@
-module DogeCoin
-  module Version
-    MAJOR = 0
-    MINOR = 0
-    PATCH = 1
-    BUILD = nil
+# module DogeCoin
+#   module Version
+#     MAJOR = 0
+#     MINOR = 0
+#     PATCH = 1
+#     BUILD = nil
 
-    class << self
-      # returns 'MAJOR.MINOR.PATCH'
-      # or 'MAJOR.MINOR.PATCH.BUILD' if BUILD is not nil
-      def to_s
-        [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
-      end
-    end
-  end
+#     class << self
+#       # returns 'MAJOR.MINOR.PATCH'
+#       # or 'MAJOR.MINOR.PATCH.BUILD' if BUILD is not nil
+#       def to_s
+#         [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+#       end
+#     end
+#   end
 
-  VERSION = Version.to_s
-end
+#   VERSION = Version.to_s
+# end
