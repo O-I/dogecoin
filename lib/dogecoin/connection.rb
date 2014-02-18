415e7049dea52bc341a8d3c35f7bcae4fb4b5040
Rahul Horé <hore.rahul@gmail.com>
2014-02-17 20:10:48 -0500

    Adds a basic connection.rb

---
+++ lib/dogecoin/connection.rb
@@ -0,0 +1,22 @@
+require 'faraday_middleware'
+
+module Dogecoin
+  module Connection
+    private
+
+    def connection
+      options = {
+        ssl: { verify: false },
+        url: 'https://dogeapi.com/wow/?'
+      }
+
+      Faraday.new(options) do |connection|
+        connection.use Faraday::Request::UrlEncoded
+        connection.use Faraday::Response::RaiseError
+        connection.use Faraday::Response::Mashify
+        connection.use Faraday::Response::ParseJson
+        connection.adapter(Faraday.default_adapter)
+      end
+    end
+  end
+end
